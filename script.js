aeb911a534e577c0449c9fb0e1875d99b7af0214
Branko Cvetanović <cbranko01@gmail.com>
2023-02-26 01:16:39 +0100

    make function for playing game 5 times

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ function getComputerChoice(){
     }
 }
 function playRound(playerSelect, compSelect){
-    playerSelect=toLowerCase(playerSelect)
+    let win =undefined
+    playerSelect=playerSelect.toLowerCase()
     if (playerSelect=="rock"&&compSelect=="paper"){
         return ("You lose! Paper beats Rock")
     }
     else if (playerSelect=="rock"&&compSelect=="scissors"){
-        return ("You win! Rock beats Scissors")
+        return ("You win! Rock beats Scissor")
     }
     else if (playerSelect=="paper"&&compSelect=="scissors"){
         return ("You lose! Scissors beats Paper")
@@ -32,13 +33,29 @@ function playRound(playerSelect, compSelect){
     }
     else{
         return("It is a draw!")
+    }   
+}
+function game(){
+    let counter1=0
+    let counter2=0
+    for (let i=0; i<5;i++){
+        let compSelect=getComputerChoice()
+        let playerSelect=prompt("Chouse ")
+        let round=playRound(playerSelect,compSelect)
+        console.log(round)
+        let winner=round.charAt(4)
+        if (winner=="w"){
+            counter1++
+        }
+        else if (winner=="l"){
+            counter2++    
     }
-
-    
-    
+    }
+    if(counter1>counter2){
+        return("covek je pobedio")
+    }
+    else if(counter1<counter2){
+        "Masina je pobedila"
+    }
 }
 
-
-
-function muda(x,y){
-}
